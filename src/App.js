883229ccabc6e5b985bedfc185a0acfd9783c0b6
catupik <gnedyshevaeg@gmail.com>
2023-10-26 13:07:25 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import { useEffect, useState } from 'react';
-import video from './video.mp4'
-import './App.css';
+import { useEffect, useState } from "react";
+import video from "./video.mp4";
+import "./App.css";
 
 function App() {
+  const [myAdvice, setMyAdvice] = useState("");
 
-  const [myAdvice, setMyAdvice] = useState('');
-
-  useEffect(()=>{
+  useEffect(() => {
     getAdvice();
-  }, [])
+  }, []);
 
-  const getAdvice = async()=> {
-    try{
+  const getAdvice = async () => {
+    try {
       const response = await fetch(`https://www.boredapi.com/api/activity/`);
       const data = await response.json();
       setMyAdvice(data.activity);
-    } catch(error){
-      console.error('Couldnt get data, error')
+    } catch (error) {
+      console.error("Couldnt get data, error");
     }
-   
-  }
+  };
   return (
     <div className="App">
       <div>
         <video playsInline autoPlay muted loop>
-        <source src={video} type='video/mp4'></source>
+          <source src={video} type="video/mp4"></source>
         </video>
       </div>
 
       <div className="advice">
-        <div className='container'>
-        <button onClick={getAdvice}>kill the boredom</button>
+        <div className="container">
+          <button onClick={getAdvice}>kill the boredom</button>
         </div>
-      <h2>{myAdvice}</h2>
+        <div className="container">
+          <h2>{myAdvice}</h2>
+        </div>
       </div>
-
     </div>
-
-
   );
 }
 
